40a2593ce91d9efd152d71e636b976264075d4ac
louis N <louis_n@naver.com>
2023-07-15 00:48:31 +0900

    230714

--- 2023_07/230714_01.py
+++ 2023_07/230714_01.py
@@ -2,6 +2,36 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120850
 
 
-def solution(my_string):
+# isdigit() 사용
+def solution_01(my_string):
     answer = []
+    for i in my_string:
+        if i.isdigit():
+            answer.append(int(i))
+    answer.sort()
+    return answer
+
+
+# try/except 사용
+def solution_02(my_string):
+    answer = []
+    for i in my_string:
+        try:
+            answer.append(int(i))
+        except:
+            pass
+    answer.sort()
+    return answer
+
+
+# 정규표현식 사용
+import re
+
+
+def solution_02(my_string):
+    answer = []
+    for i in my_string:
+        if re.match(r"^\d+$", i):
+            answer.append(int(i))
+    answer.sort()
     return answer
