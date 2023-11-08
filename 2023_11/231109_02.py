b7a5cb4e433ebd7ea03d78cf1765e733abb9b7d6
louis N <louis_n@naver.com>
2023-11-09 01:17:11 +0900

    231109

--- 2023_11/231109_02.py
+++ 2023_11/231109_02.py
@@ -18,7 +18,7 @@ def solution_01(num, k):
 # 풀이_02 -> 반복문, enumerate() 사용
 
 
-def solution(num, k):
+def solution_02(num, k):
     answer = -1
     num_list = list(map(int,str(num)))
     for i,value in enumerate(num_list):
