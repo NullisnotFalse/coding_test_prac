b13b035c82ca175a05fa68ee2d2cd32d7bd1992c
louisN <louis_n@naver.com>
2023-11-29 00:44:49 +0900

    20231128

--- 2023_11/231128_01.py
+++ 2023_11/231128_01.py
@@ -30,3 +30,29 @@ def solution(strings, n):
 테스트 11 〉 통과 (0.01ms, 10.3MB)
 테스트 12 〉 통과 (0.03ms, 10.1MB)
 """
+
+
+# 풀이_02 -> 정렬만 활용
+
+
+def solution(strings, n):
+    strings.sort()
+    answer = sorted(strings, key=lambda x: x[n])
+    return answer
+
+
+"""
+실행결과
+테스트 1 〉	통과 (0.00ms, 10.1MB)
+테스트 2 〉	통과 (0.01ms, 10MB)
+테스트 3 〉	통과 (0.01ms, 10.1MB)
+테스트 4 〉	통과 (0.02ms, 10.1MB)
+테스트 5 〉	통과 (0.01ms, 9.98MB)
+테스트 6 〉	통과 (0.01ms, 10.3MB)
+테스트 7 〉	통과 (0.01ms, 10.2MB)
+테스트 8 〉	통과 (0.01ms, 10.1MB)
+테스트 9 〉	통과 (0.01ms, 10.2MB)
+테스트 10 〉 통과 (0.01ms, 10MB)
+테스트 11 〉 통과 (0.00ms, 10.1MB)
+테스트 12 〉 통과 (0.02ms, 10.1MB)
+"""
